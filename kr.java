240d8719507c83356b204ea41aef46db3394dba0
aselyatk <133993143+aselyatk@users.noreply.github.com>
2023-10-26 20:44:46 +0600

    Update kr.java

--- kr.java
+++ kr.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.time.LocalDateTime;
+import java.time.LocalDateTime;  //библиотека
 
 public class kr {
     public static void main(String[] args) throws IOException {
